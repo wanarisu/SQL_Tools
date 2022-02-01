a8433b3fb09a18180c8f9b45e7cd6bd2da593d43
wanarisu <wanarisu@gamil.com>
2022-02-01 09:04:32 +0900

    masterBase (6.1. Aceの基本 230-250)
    ウィンドを閉じるイベントまで

--- index.js
+++ index.js
@@ -12,28 +12,80 @@ function createWindow() {
     });
 
     win.loadFile('index.html');
+    win.webContents.openDevTools();
     return win.id;
 }
 
 function createMenu() {
     let menu_temp = [{
             label: 'File',
-            submenu: [{
-                    label: 'New',
-                    click: () => {
-                        createWindow();
-                    }
-                },
+            submenu: [
+                { label: 'New', click: () => { createWindow(); } },
+                { label: 'Open folder...', click: () => { openfolder(); } },
                 { role: 'close' },
                 { type: 'separator' },
                 { role: 'quit' }
             ]
         },
         { role: 'editMenu' },
+        {
+            label: 'Theme',
+            submenu: [
+                { label: 'textmate', click: () => setTheme('textmate') },
+                { label: 'chrome', click: () => setTheme('chrome') },
+                { label: 'github', click: () => setTheme('github') },
+                { label: 'dracula', click: () => setTheme('dracula') },
+                { label: 'twilight', click: () => setTheme('twilight') },
+                { label: 'pastel on dark', click: () => setTheme('pastel_on_dark') }
+            ]
+        },
+        {
+            label: 'Mode',
+            submenu: [
+                { label: 'text', click: () => setMode('text') },
+                { label: 'javascript', click: () => setMode('javascript') },
+                { label: 'html', click: () => setMode('html') },
+                { label: 'python', click: () => setMode('python') },
+                { label: 'php', click: () => setMode('php') }
+            ]
+        },
+        {
+            label: 'Font',
+            submenu: [
+                { label: '9', click: () => setFontSize(9) },
+                { label: '10', click: () => setFontSize(10) },
+                { label: '12', click: () => setFontSize(12) },
+                { label: '14', click: () => setFontSize(14) },
+                { label: '16', click: () => setFontSize(16) },
+                { label: '18', click: () => setFontSize(18) },
+                { label: '20', click: () => setFontSize(20) },
+                { label: '24', click: () => setFontSize(24) }
+            ]
+        }
     ];
 
     let menu = Menu.buildFromTemplate(menu_temp);
     Menu.setApplicationMenu(menu);
+}
+
+function setTheme(theme) {
+    let w = BrowserWindow.getFocusedWindow();
+    w.webContents.executeJavaScript('setTheme("' + theme + '")');
+}
+
+function setMode(mname) {
+    let w = BrowserWindow.getFocusedWindow();
+    w.webContents.executeJavaScript('setMode("' + mname + '")');
+}
+
+function setFontSize(n) {
+    let w = BrowserWindow.getFocusedWindow();
+    w.webContents.executeJavaScript('setFontSize(' + n + ')');
+}
+
+function openfolder() {
+    let w = BrowserWindow.getFocusedWindow();
+    w.webContents.executeJavaScript('openfolder() ');
 }
 
 createMenu();
--- preload.js
+++ preload.js
@@ -1,6 +1,10 @@
 const { remote } = require('electron');
 const { dialog, BrowserWindow } = remote;
+const fs = require('fs');
+const path = require('path');
 
 window.remote = remote;
 window.BrowserWindow = BrowserWindow;
 window.dialog = dialog;
+window.fs = fs;
+window.path = path;
